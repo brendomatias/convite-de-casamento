6a5bdc3730bea9267473d926827a791b08e6fe0d
brendomatias <brendonmatias.dev@gmail.com>
2026-02-27 12:06:39 -0300

    add localização e telefone

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function abrirCarta(){
     trocaImagem.src = "/fotos/detalhes.jpg"
 }
 
+function abrirTelefone(){
+    window.location.href = "https://Wa.me/+558398038323"
+}
+function abrirLocalização(){
+    window.location.href = "https://maps.app.goo.gl/j1Q1bjNAjwovxHKf6?g_st=iw"
+}
